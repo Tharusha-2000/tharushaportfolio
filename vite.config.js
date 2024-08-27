6b029bc7a2f7b0938d2c94a4bd44c76ef7024160
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-08-27 16:47:32 +0530

    add viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disable minification
+    sourcemap: true,
+  },
+
 })
